9d2d523f2fd57ec5682547dba958465536f63bad
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2023-06-19 18:57:55 +0530

    All things dded

--- To_Do_Litst/index.js
+++ To_Do_Litst/index.js
@@ -10,7 +10,29 @@ submit_btn.addEventListener("click",function()
  else{
     let li=document.createElement("li");
     li.innerHTML=val.value;
+    let edit=document.createElement("button");
+    let clear=document.createElement("button");
+    clear.innerHTML="Clear"
+    //Yha se bnaya hai
+    edit.innerHTML="Edit"
     container.appendChild(li);
+    li.appendChild(edit);
+    li.appendChild(clear);
  }
- val.value="";
+ val.value=""; 
+ let clear=document.querySelector(".clear-btn");
+if(container.children.length>=1)
+{
+   clear.classList.add("show");
+}
+})
+let clearall=document.querySelector(".clear-btn");
+clearall.addEventListener("click",function()
+{ 
+   while(container.firstChild)
+   {
+      container.removeChild(container.firstChild);
+   }
+   clearall.style.display='none';
+   val="";
 })
